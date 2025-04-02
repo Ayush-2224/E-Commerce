ad44e9b1cba421f45400c604e2eeba2cf44cffcb
Kumar Ayaman <142806861+kr-Ayaman@users.noreply.github.com>
2025-04-02 22:45:00 +0530

    review complete

---
+++ Backend/src/controllers/reviews.controller.js
@@ -0,0 +1,74 @@
+import Review from '../models/review.model.js';
+import HttpError from '../models/http-error';
+
+const addReview = async (req, res, next) => {
+    try{
+        const {review, rating, productId} = req.body;
+        const {userId} = req.userData._id;
+        const newReview = new Review({
+            review,
+            rating,
+            productId,
+            userId
+        });
+        await newReview.save();
+    }catch(error){
+        console.log("add review error: ", error);
+        return next(new HttpError("An error occurred while adding the review", 500));
+    }
+}
+const getReviews = async (req, res, next) =>{
+    try{
+        const {productId, sortBy, num} = req.params;
+        const reviews = await Review.find({productId})
+                        .sort({[sortBy]: num})
+                        .populate("userId", "username profilePic")
+                        .select("review rating userId - _id");
+        res.json(reviews)
+    }catch(error){
+        print(error);
+    }
+}
+
+const changeReview = async (req, res, next) => {
+    try{
+        const {review, rating, productId} = req.body;
+        const {userId} = req.userData._id;
+        const updatedReview = await Review.findOneAndUpdate({productId, userId}, {
+            review,
+            rating
+        }, {new: true});
+        res.json(updatedReview);
+    }catch(error){
+        console.log("change review error: ", error);
+        return next(new HttpError("An error occurred while changing the review", 500));
+    }
+}
+
+const deleteReview = async (req, res, next) =>{
+    const {productId} = req.params;
+    const userId = req.userData._id;
+    try{
+        await Review.findOneAndDelete({productId, userId});
+        res.status(200).json({message: "Review deleted successfully"});
+    }catch(error){
+        console.log("delete review error: ", error);
+        return next(new HttpError("An error occurred while deleting the review", 500));
+    }
+}
+
+const getUserReviews = async (req, res, next) => {
+    const userId = req.userData._id;
+    try {
+        const reviews = await Review.find({ userId })
+            .select("productId rating review - _id"); 
+        
+        res.json(reviews);
+    } catch (error) {
+        console.log("get user reviews error: ", error);
+        return next(new HttpError("An error occurred while getting the user reviews", 500));
+    }
+};
+
+
+export {addReview, getReviews, changeReview, deleteReview}
--- Backend/src/models/product.model.js
+++ Backend/src/models/product.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const descriptionElementSchema = mongoose.Schema({
     type:{
         type:String,
