3014053335c587bdd77f614987ae53a38818cd3e
Kumar Ayaman <142806861+kr-Ayaman@users.noreply.github.com>
2025-04-14 00:32:55 +0530

    aa

--- Frontend/src/components/cartProduct.jsx
+++ Frontend/src/components/cartProduct.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const cartProduct = props => {
   return (
     <div>
-        <div>
-            <h3></h3>
-        </div>
+            <img src={props.imageUrl} alt="a" />
+            <div>
+                <h3>{props.title}</h3>
+                <p>{props.price}</p>
+                <p>{props.mrp}</p>
+                <p>Seller: {props.seller}</p>
+            </div>
     </div>
   )
 }
--- Frontend/src/pages/cart.jsx
+++ Frontend/src/pages/cart.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { axiosInstance } from '../lib/axios'
 
 const cart = () => {
-    const Cart = axiosInstance
+    const [cart, setCart] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const[error, setError] = useState(null)
+    useEffect(() =>{
+        const fetchCart = async () =>{
+            setLoading(true)
+            try{
+                const response = await axiosInstance.get("/getCart")
+                setCart(response.data.cart)
+            }
+        }
+    })
 
   return (
     <div>
